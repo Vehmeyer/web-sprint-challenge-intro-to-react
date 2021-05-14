6260d0db8f6162964862d73958fff600bc3eca90
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-14 08:15:36 -0500

    successfully fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Header from './components/Header';
@@ -8,10 +8,24 @@ import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const[character, setCharacter] = useState([]);
+  const[characterList, setCharacterList] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")       
+     .then(res => {
+       console.log(res.data) 
+      //  setCharacterList(res.characterList)
+        })
+     .catch((err) => {
+       debugger
+       })
+ }, []);
+
 
   return (
     <div className="App">
